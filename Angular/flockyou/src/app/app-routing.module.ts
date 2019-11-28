6c37131177f89b3aaaae2d93c270f9d54cbee7c6
Maxime Larrieu <maximelarrieu@github.com>
2019-11-28 09:06:10 +0100

    :first page&route:

--- Angular/flockyou/src/app/app-routing.module.ts
+++ Angular/flockyou/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', component: MainComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
   exports: [RouterModule]
 })
--- Angular/flockyou/src/app/app.module.ts
+++ Angular/flockyou/src/app/app.module.ts
@@ -8,9 +8,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {MainComponent} from "./main/main.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+      AppComponent,
+      MainComponent,
+      ProfileComponent
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
